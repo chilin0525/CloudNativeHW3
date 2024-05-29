d1c2e1d54e13ac31d769b7e377783dfc7fe35510
Chilin Chiou <chilin.chiou@gmail.com>
2024-05-29 11:58:30 +0800

    change config to generate coverage html

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    pool: 'forks'
+    pool: 'forks',
+    coverage: {
+      enabled: true,
+      reportsDirectory: 'coverage',
+      reporter: ['html', 'lcov', 'text-summary'], // ensures multiple output formats
+    },
   }
 })
